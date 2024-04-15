605e6f53ea1aa3c0ab437306922b8f81c7168d90
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 17:14:22 +0200

    add createSessionZodSchema

---
+++ src/zodSchema/session.zodSchema.ts
@@ -0,0 +1,17 @@
+import z from 'zod'
+
+const createSessionZodSchema = z.object({
+  body: z.object({
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email('Not a valid email'),
+    password: z
+      .string({
+        required_error: 'Password is required'
+      })
+      .min(6, 'Password too short - should be 6 chars minimum')
+  })
+})
+
+export { createSessionZodSchema }
+export type TCreateSessionInput = z.TypeOf<typeof createSessionZodSchema>
