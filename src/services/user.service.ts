a9af4d6773a45752de68ec6d7b68046d506934b3
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 21:36:12 +0200

    add getUser + checkPassword

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,14 @@
 import { omit } from 'lodash'
-import User from '../models/user.model'
-import AppError from '../utils/AppError'
+import User, { IUserDocument } from '../models/user.model'
+import AppError from '../utils/AppError.utils'
 import type { TCreateUserInput } from '../zodSchema/user.zodSchema'
+import type { Types } from 'mongoose'
+
+export async function getUser(userId: Types.ObjectId) {
+  const user = await User.findById(userId)
+
+  return user
+}
 
 export async function createUser(inputData: TCreateUserInput['body']) {
   try {
@@ -15,4 +22,33 @@ export async function createUser(inputData: TCreateUserInput['body']) {
       throw err
     }
   }
+}
+
+export async function checkPassword({
+  email,
+  password
+}: {
+  email: string
+  password: string
+}): Promise<Omit<IUserDocument, 'password' | 'comparePassword'> | null> {
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    return null
+  }
+
+  const isValid = await (user as IUserDocument).comparePassword(password)
+  if (!isValid) {
+    return null
+  }
+
+  const userWithoutPassword = user.toObject({
+    transform: (doc, ret) => {
+      delete ret.password
+      delete ret.comparePassword
+      return ret
+    }
+  }) as Omit<IUserDocument, 'password' | 'comparePassword'>
+
+  return userWithoutPassword
 }
