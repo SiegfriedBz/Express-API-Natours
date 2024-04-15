800d26c46016cab9b4061deac8a301b76085ce8e
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 21:35:04 +0200

    move to middlewares

---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,94 @@
+import AppError from '../utils/AppError.utils'
+import { ZodError } from 'zod'
+import type { NextFunction, Request, Response } from 'express'
+
+export default function errorMiddleware(
+  err: unknown,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) {
+  let error = err
+
+  // Zod ValidateRequest thrown error(s)
+  if (error instanceof ZodError) {
+    const errorMessage = []
+    for (const issue of error.issues) {
+      errorMessage.push(issue.message)
+    }
+    if (errorMessage.length > 0) {
+      error = handleZodError(errorMessage.join(', '))
+    }
+  }
+
+  // Mongo thrown error on DUP KEY
+  if (error instanceof AppError && error.code === 11000) {
+    if (
+      typeof error === 'object' &&
+      error != null &&
+      'keyValue' in error &&
+      error.keyValue != null &&
+      typeof error.keyValue === 'object' &&
+      'name' in error.keyValue
+    ) {
+      error = handleDupKeyDBError(error as { keyValue: { name: string } })
+    }
+  }
+
+  // Mongo thrown error on INVALID DB ID
+  if ((error as AppError) && AppError.name === 'CastError') {
+    if (
+      typeof error === 'object' &&
+      error !== null &&
+      'path' in error &&
+      'value' in error
+    ) {
+      error = handleCastDBError(error as { path: string; value: string })
+    }
+  }
+
+  /** Send Response */
+  if (error instanceof AppError) {
+    res.status(error.statusCode).json({
+      status: error.statusText,
+      error: { message: error.message }
+    })
+  } else {
+    res.status(500).json({
+      status: 'error',
+      error: { message: 'Something went wrong' }
+    })
+  }
+}
+
+/** Helpers */
+//
+const handleZodError = (message: string) => {
+  const error = new AppError({ statusCode: 400, message })
+
+  return error
+}
+
+// DB Errors
+const handleCastDBError = (err: { path: string; value: string }) => {
+  const message = `Invalid ${err.path}: ${err.value}`
+  const error = new AppError({ statusCode: 400, message })
+
+  error.isHandledDBError = true
+
+  return error
+}
+
+const handleDupKeyDBError = (err: { keyValue: { name: string } }) => {
+  const fieldError =
+    err?.keyValue?.name ||
+    Object.keys(err?.keyValue)
+      ?.map((key) => key)
+      ?.join(', ')
+  const message = `Duplicate field value on: ${fieldError}. Please use another value`
+  const error = new AppError({ statusCode: 400, message })
+  error.isHandledDBError = true
+
+  return error
+}
