7566fb2f836270d0e5b486e7d941df3f49490ec9
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 16:44:28 +0200

    add jest supertest mongodb-memory-server

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.test.ts'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true
+}
+
+export default config
