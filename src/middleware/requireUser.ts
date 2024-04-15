801fd1269efb321144b45eb1ff1d35350220069c
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 21:27:22 +0200

    add requireUser middleware

---
+++ src/middleware/requireUser.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from 'express'
+import AppError from '../utils/AppError.utils'
+
+export default async function requireUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // after deserializeAndRefreshUser middleware
+  const userId = res.locals?.user?._id
+
+  if (!userId) {
+    return next(
+      new AppError({
+        statusCode: 403,
+        message: `Please login to access this resource`
+      })
+    )
+  }
+
+  next()
+}
