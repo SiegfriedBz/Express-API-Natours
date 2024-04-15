60e14e54070ad7fb2405324e28eb1e4d7ffabc95
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 21:30:28 +0200

    add deserializeAndRefreshUser to all routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
+import deserializeAndRefreshUser from '../middleware/deserializeAndRefreshUser'
 import userRoutes from './user.route'
 import sessionRoutes from './session.route'
 import type { Express, Request, Response } from 'express'
 
 export default function routes(app: Express) {
+  app.use(deserializeAndRefreshUser)
+
   app.get('/api/healthcheck', (req: Request, res: Response) => {
     return res.sendStatus(200)
   })
