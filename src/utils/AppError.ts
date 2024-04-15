dc3e2518d4e2142f675a27ed164529fb0c00eddb
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 13:23:53 +0200

    add AppError errorMiddleware logger

---
+++ src/utils/AppError.ts
@@ -0,0 +1,22 @@
+export default class AppError extends Error {
+  statusCode: number
+  statusText: 'success' | 'fail' | 'error'
+  isHandledDBError: boolean
+  code: number | null
+  name: string
+
+  constructor({
+    statusCode,
+    message
+  }: {
+    statusCode: number
+    message: string
+  }) {
+    super(message)
+    this.statusCode = statusCode
+    this.statusText = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
+    this.isHandledDBError = false
+    this.code = null
+    this.name = ''
+  }
+}
