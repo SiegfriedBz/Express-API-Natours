5a8b20790ca8d7f280feb5c5d9aa837677377e0f
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-15 21:30:01 +0200

    fix user schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -10,30 +10,39 @@ export interface IUserInput {
 export interface IUserDocument extends IUserInput, mongoose.Document {
   createdAt: Date
   updatedAt: Date
+  comparePassword: (candidatePassword: string) => Promise<boolean>
 }
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, unique: true, required: true },
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    name: { type: String, required: true },
     password: { type: String, required: true }
   },
   { timestamps: true }
 )
 
-userSchema.pre('save', async (next) => {
-  const user = this as unknown as IUserDocument
-
-  if (!user?.isModified('password')) {
+// pre-hook
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
     return next()
   }
 
   const salt = await bcrypt.genSalt(config.get<number>('bcrypt.saltWorkFactor'))
-  const hashPassword: string = await bcrypt.hash(user.password, salt)
+  const hashPassword: string = await bcrypt.hash(this.password, salt)
 
-  user.password = hashPassword
+  this.password = hashPassword
 
   next()
 })
 
-export default mongoose.model('User', userSchema)
+// Instance methods
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  const user = this as IUserDocument
+
+  return bcrypt.compare(candidatePassword, user.password).catch(() => false)
+}
+
+export default mongoose.model<IUserDocument>('User', userSchema)
