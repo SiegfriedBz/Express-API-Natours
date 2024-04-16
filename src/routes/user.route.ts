34245af42fe8e681d1d4c8db7084af3de1c2ed4b
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-16 14:23:58 +0200

    update middlewares + add admin updateUser route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,28 @@
 import express from 'express'
-import { createUserHandler } from '../controllers/user.controller'
+import {
+  createUserHandler,
+  updateUserHandler
+} from '../controllers/user.controller'
 import validateRequest from '../middleware/validateRequest'
-import { createUserZodSchema } from '../zodSchema/user.zodSchema'
+import requireUser from '../middleware/requireUser'
+import restrictTo from '../middleware/restrictTo'
+import {
+  createUserZodSchema,
+  adminUpdateUserZodSchema
+} from '../zodSchema/user.zodSchema'
+
 const router = express.Router()
 
+// User Signup
 router.route('/').post(validateRequest(createUserZodSchema), createUserHandler)
+
+// User-protected routes
+router.use(requireUser)
+
+// Admin-protected routes
+router.use(restrictTo('admin'))
+router
+  .route('/:userId')
+  .put(validateRequest(adminUpdateUserZodSchema), updateUserHandler)
 
 export default router
