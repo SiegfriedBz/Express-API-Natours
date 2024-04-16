a4bbeb72bb3b5ddb34214ec3f146914cae2017c9
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-16 14:26:08 +0200

    add updateUser handler + service

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,10 @@
-import { createUser } from '../services/user.service'
+import { createUser, getUser, updateUser } from '../services/user.service'
 import type { Request, Response, NextFunction } from 'express'
-import type { TCreateUserInput } from '../zodSchema/user.zodSchema'
+import type {
+  TAdminUpdateUserInput,
+  TCreateUserInput
+} from '../zodSchema/user.zodSchema'
+import AppError from '../utils/AppError.utils'
 
 /** Signup */
 export const createUserHandler = async (
@@ -16,6 +20,41 @@ export const createUserHandler = async (
     return res.status(201).json({
       status: 'success',
       data: { user }
+    })
+  } catch (err: unknown) {
+    next(err)
+  }
+}
+
+/** Admin update user */
+export const updateUserHandler = async (
+  req: Request<
+    TAdminUpdateUserInput['params'],
+    object,
+    TAdminUpdateUserInput['body']
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {
+      body,
+      params: { userId }
+    } = req
+
+    // Check user exists
+    const user = await getUser(userId)
+
+    if (!user) {
+      return next(new AppError({ statusCode: 404, message: 'User not found' }))
+    }
+
+    // Update user
+    const updatedUser = await updateUser({ _id: userId }, body)
+
+    return res.status(200).json({
+      status: 'success',
+      data: { user: updatedUser }
     })
   } catch (err: unknown) {
     next(err)
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,15 +1,16 @@
 import { omit } from 'lodash'
-import User, { IUserDocument } from '../models/user.model'
+import User from '../models/user.model'
 import AppError from '../utils/AppError.utils'
+import type { FilterQuery, UpdateQuery } from 'mongoose'
 import type { TCreateUserInput } from '../zodSchema/user.zodSchema'
-import type { Types } from 'mongoose'
+import type { IUserDBInput, IUserDocument } from '../types/user.types'
 
-export async function getUser(userId: Types.ObjectId) {
+export async function getUser(userId: string) {
   const user = await User.findById(userId)
 
   return user
 }
-
+/** Signup */
 export async function createUser(inputData: TCreateUserInput['body']) {
   try {
     const newUser = await User.create(inputData)
@@ -22,6 +23,16 @@ export async function createUser(inputData: TCreateUserInput['body']) {
       throw err
     }
   }
+}
+
+/** Admin update user */
+export async function updateUser(
+  filter: FilterQuery<IUserDocument>,
+  update: UpdateQuery<IUserDBInput>
+) {
+  const updatedUser = await User.findOneAndUpdate(filter, update, { new: true })
+
+  return updatedUser ? omit(updatedUser.toObject(), 'password') : null
 }
 
 export async function checkPassword({
