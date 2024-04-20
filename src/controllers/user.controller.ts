f671660a806510abeeb28804d09556cc1bd276d2
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-20 15:40:03 +0200

    update user + session controllers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,15 @@
+import { omit } from 'lodash'
 import {
   checkPassword,
   createUser,
+  getAllUsers,
   getUser,
   updateUser
 } from '../services/user.service'
 import { generateTokens } from '../services/session.service'
-import AppError from '../utils/AppError.utils'
 import setTokenCookieOptions from '../utils/setTokenCookieOptions.utils'
+import AppError from '../utils/AppError.utils'
+import logger from '../utils/logger.utils'
 import type { Request, Response, NextFunction } from 'express'
 import type {
   TAdminUpdateUserInput,
@@ -15,7 +18,6 @@ import type {
   TUpdateMyPasswordInput
 } from '../zodSchema/user.zodSchema'
 import type { IUserDocument } from '../types/user.types'
-import { omit } from 'lodash'
 
 /** User Signup */
 export const createUserHandler = async (
@@ -33,24 +35,70 @@ export const createUserHandler = async (
       data: { user }
     })
   } catch (err: unknown) {
+    logger.error(err)
     next(err)
   }
 }
 
-/** User update self - except password */
+/** Protected
+ *  User get self
+ */
+export const getMeHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const currentUserId = res.locals.user._id
+
+    const currentUser = await getUser(currentUserId)
+
+    if (!currentUser) {
+      return next(
+        new AppError({
+          statusCode: 404,
+          message: 'User not found'
+        })
+      )
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      data: { user: currentUser }
+    })
+  } catch (err: unknown) {
+    logger.error(err)
+    next(err)
+  }
+}
+
+/** Protected
+ *  User update self - except password
+ */
 export const updateMeHandler = async (
   req: Request<object, object, TUpdateMeInput['body']>,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const currentUserId = res.locals.user
-    const currentSessionId = res.locals.sessionId
-
     const { body } = req
 
-    // 1. Update user
-    const updatedUser = await updateUser({ _id: currentUserId }, body)
+    // Get userId & sessionId from prev middlewares (deserializeAndRefreshUser -> requireUser)
+    const currentUserId = res.locals.user._id
+    const currentSessionId = res.locals.sessionId
+
+    // Get all file names (if any) from multer upload middleware
+    const userImageFileName = res.locals.userImageFileName
+    const userPhotoName = userImageFileName?.photo
+
+    // Populate data for update
+    const data = {
+      ...body,
+      ...(userPhotoName ? { photo: userPhotoName } : {})
+    }
+
+    // Update user
+    const updatedUser = await updateUser({ _id: currentUserId }, data)
 
     if (!updatedUser) {
       return next(
@@ -61,37 +109,47 @@ export const updateMeHandler = async (
       )
     }
 
-    // 2. Generate new access + refresh Tokens
+    // Generate new access + refresh Tokens
     const { accessToken, refreshToken } = generateTokens({
       user: updatedUser,
       sessionId: currentSessionId
     })
 
-    // 3. Set cookies & response
-    res.cookie('accessToken', accessToken, setTokenCookieOptions('accessToken'))
+    // Set cookies & response
+    res.cookie(
+      'accessToken',
+      accessToken,
+      setTokenCookieOptions()
+      // 'accessToken'
+    )
     res.cookie(
       'refreshToken',
       refreshToken,
-      setTokenCookieOptions('refreshToken')
+      setTokenCookieOptions()
+      // 'refreshToken'
     )
 
-    // 4. Send response
+    // Send response
     return res.status(200).json({
       status: 'success',
       data: { user: updatedUser }
     })
   } catch (err: unknown) {
+    logger.error(err)
     next(err)
   }
 }
 
-/** User update self-password */
+/** Protected
+ *  User update self-password
+ */
 export const updateMyPasswordHandler = async (
   req: Request<object, object, TUpdateMyPasswordInput['body']>,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    // Get userEmail from prev middlewares (deserializeAndRefreshUser -> requireUser)
     const currentUserEmail = res.locals.user.email
 
     const {
@@ -134,11 +192,14 @@ export const updateMyPasswordHandler = async (
       data: { user: userWithoutPassword }
     })
   } catch (err: unknown) {
+    logger.error(err)
     next(err)
   }
 }
 
-/** Admin update user */
+/** Protected
+ *  Admin
+ */
 export const updateUserHandler = async (
   req: Request<
     TAdminUpdateUserInput['params'],
@@ -171,6 +232,63 @@ export const updateUserHandler = async (
       data: { user: updatedUser }
     })
   } catch (err: unknown) {
+    logger.error(err)
+    next(err)
+  }
+}
+
+/** Protected
+ *  Admin
+ */
+export const getAllUsersHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await getAllUsers()
+
+    return res.status(200).json({
+      status: 'success',
+      dataCount: users?.length,
+      data: { users }
+    })
+  } catch (err: unknown) {
+    logger.error(err)
+    next(err)
+  }
+}
+
+/** Protected
+ *  Admin
+ */
+export const getUserHandler = async (
+  req: Request<{ userId: string }, object, object>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {
+      params: { userId }
+    } = req
+
+    const user = await getUser(userId)
+
+    if (!user) {
+      return next(
+        new AppError({
+          statusCode: 404,
+          message: 'User not found'
+        })
+      )
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      data: { user }
+    })
+  } catch (err: unknown) {
+    logger.error(err)
     next(err)
   }
 }
