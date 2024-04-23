fbd862e1627fc12905a94fe4af074b5b8b4854f7
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-23 19:12:57 +0200

    update .env + config for mail

--- config/development.ts
+++ config/development.ts
@@ -1,9 +1,1 @@
-export default {
-  cors: { allowedOrigins: 'http://localhost:3000' },
-  stripe: {
-    stripePrivateKey:
-      'sk_test_51OATwgGyO055z3D9eOYJ73jSMWPYmaMdin20BMynmAPGEv2DwLP57ctckumiJoaYznVp1DmMLQu5uF8ADWtJldjx00mPdzCceU',
-    stripePublicKey:
-      'pk_test_51OATwgGyO055z3D9vjQz2YQXdxrmvqMP7aQpGTfVEyurUFVNDtDLIdhMnPFRJmCabQiYFFQ0Lrkz3TLBioAo1QIk00bKqGaT8C'
-  }
-}
+export default {}
