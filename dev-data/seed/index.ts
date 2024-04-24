a4198c32bc6ffd1b02df210b50d9801a7686edd4
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-24 11:35:12 +0200

    refactor seed

--- dev-data/seed/index.ts
+++ dev-data/seed/index.ts
@@ -5,12 +5,15 @@ import User from '../../src/models/user.model'
 import Tour from '../../src/models/tour.model'
 import Review from '../../src/models/review.model'
 import Booking from '../../src/models/booking.model'
-
 import logger from '../../src/utils/logger.utils'
 import { readFilePromise } from '../../src/utils/fs.utils'
 import type { TCreateTourInput } from '../../src/zodSchema/tour.zodSchema'
 import type { TCreateUserInput } from '../../src/zodSchema/user.zodSchema'
 import type { TCreateReviewInput } from '../../src/zodSchema/review.zodSchema'
+import type { ITourDocument } from '../../src/types/tour.types'
+import type { IUserDocument } from '../../src/types/user.types'
+import type { IReviewDocument } from '../../src/types/review.types'
+import type { IBookingDocument } from '../../src/types/booking.types'
 
 type TCreateTourSeed = {
   _id: string
@@ -30,98 +33,48 @@ const ENV = process.env.NODE_ENV
 const dbUri = config.get<string>('dbUri')
 
 const seed = async () => {
-  /** 1. connect to MongoDB */
+  /** 1. Connect to MongoDB */
+  logger.info(`*******`)
+  logger.info(`Connecting to ${ENV} DB...`)
   const connected = await mongoose.connect(dbUri)
 
   if (!connected) throw new Error('❌ DB connection failed')
 
-  logger.info(`✅ ${ENV} DB connection successful`)
+  logger.info(`🚀 ${ENV} DB connection successful`)
 
-  /** 2. Delete Tours in DB */
-  logger.info('Deleting tours...')
-  await Tour.deleteMany()
-  logger.info('✅ Deleting tours..DONE')
+  /** 2. Delete & Seed Tours */
+  await seedModel<ITourDocument, TCreateTourSeed>({
+    Model: Tour as mongoose.Model<ITourDocument>,
+    name: 'tours'
+  })
 
-  /** 3. get data from fs */
-  const toursDataJson = await readFilePromise('dev-data/data/tours.json')
+  /** 3. Delete & Seed Users */
+  await seedModel<IUserDocument, TCreateUserSeed>({
+    Model: User as mongoose.Model<IUserDocument>,
+    name: 'users'
+  })
 
-  const toursData = JSON.parse(toursDataJson)
+  /** 4. Delete & Seed Reviews */
+  await seedModel<IReviewDocument, TCreateReviewSeed>({
+    Model: Review as mongoose.Model<IReviewDocument>,
+    name: 'reviews'
+  })
 
-  /** 4. Seed Tours in DB */
-  logger.info('Seeding tours...')
-  const newToursPromises = toursData.map((tour: TCreateTourSeed) =>
-    Tour.create(tour)
-  )
-  await Promise.all(newToursPromises)
-  logger.info('✅ Seeding tours..DONE')
-
-  logger.info('======')
-
-  // /** 5. Delete Users in DB */
-  logger.info('Deleting users...')
-  await User.deleteMany()
-  logger.info('✅ Deleting users..DONE')
-
-  /** 6. get  data from fs */
-  const usersDataJson = await readFilePromise('dev-data/data/users.json')
-
-  const usersData = JSON.parse(usersDataJson)
-
-  /** 7. Seed Users in DB */
-  logger.info('Seeding users...')
-  const newUsersPromises = usersData.map((user: TCreateUserSeed) =>
-    User.create(user)
-  )
-  await Promise.all(newUsersPromises)
-  logger.info('✅ Seeding users...DONE')
-
-  logger.info('======')
-
-  /** 5. Delete Reviews in DB */
-  logger.info('Deleting reviews...')
-  await Review.deleteMany()
-  logger.info('✅ Deleting reviews...DONE')
-
-  /** 6. get data from fs */
-  const reviewsDataJson = await readFilePromise('dev-data/data/reviews.json')
-
-  const reviewsData = JSON.parse(reviewsDataJson)
-
-  /** 7. Seed Reviews in DB */
-  logger.info('Seeding reviews...')
-  const newReviewsPromises = reviewsData.map((review: TCreateReviewSeed) =>
-    Review.create(review)
-  )
-  await Promise.all(newReviewsPromises)
-  logger.info('✅ Seeding reviews...DONE')
-
-  logger.info('======')
-
-  /** 8. Delete Bookings in DB */
-  logger.info('Deleting bookings...')
-  await Booking.deleteMany()
-  logger.info('✅ Deleting bookings...DONE')
-
-  /** 9. get data from fs */
-  const bookingsDataJson = await readFilePromise('dev-data/data/bookings.json')
-
-  const bookingsData = JSON.parse(bookingsDataJson)
-
-  /** 10. Seed bookings in DB */
-  logger.info('Seeding bookings...')
-  const newBookingsPromises = bookingsData.map((booking: TCreateBookingSeed) =>
-    Booking.create(booking)
-  )
-  await Promise.all(newBookingsPromises)
-  logger.info('✅ Seeding bookings...DONE')
+  /** 5. Delete & Seed Bookings */
+  await seedModel<IBookingDocument, TCreateBookingSeed>({
+    Model: Booking as mongoose.Model<IBookingDocument>,
+    name: 'bookings'
+  })
 }
 
 seed()
   .then(() => {
-    logger.info('===')
+    logger.info(`*******`)
     logger.info(
       `🚀 Cleaning ${ENV} DB & Seeding Tours, Users, Reviews and Bookings...DONE`
     )
+    logger.info(`*******`)
+    logger.info(`*******`)
     mongoose.disconnect()
     mongoose.connection.close()
     process.exit(0)
@@ -132,3 +85,29 @@ seed()
     mongoose.connection.close()
     process.exit(1)
   })
+
+/** Helper */
+type TSeedModelProps<T, U> = {
+  Model: mongoose.Model<T>
+  name: string
+  _dummy?: U
+}
+
+const seedModel = async <T, U>({ Model, name }: TSeedModelProps<T, U>) => {
+  logger.info(`*******`)
+  /** 1. Delete */
+  logger.info(`Deleting ${name}...`)
+  await Model.deleteMany()
+  logger.info(`'✅ Deleting ${name}..DONE'`)
+
+  /** 2. Get data from fs */
+  const dataJson = await readFilePromise(`dev-data/data/${name}.json`)
+  const data: U[] = JSON.parse(dataJson)
+
+  /** 3. Seed */
+  logger.info(`Seeding ${name}...`)
+  const promises = data.map((item: U) => Model.create(item))
+  await Promise.all(promises)
+  logger.info(`'✅ Seeding ${name}..DONE'`)
+  logger.info(`*******`)
+}
