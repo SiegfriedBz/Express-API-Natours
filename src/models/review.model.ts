9527ae997ba84db70382a1eeb391a45946a5160d
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-20 16:29:50 +0200

    add populate user on review schema pre find hook

--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -28,4 +28,18 @@ const reviewSchema = new mongoose.Schema(
 // prevent 1 user to write 2 reviews for the same tour
 reviewSchema.index({ user: 1, tour: 1 }, { unique: true })
 
+/** Query pre Hook - this : current Query instance */
+reviewSchema.pre(/^find/, function (next) {
+  ;(this as mongoose.Query<IReviewDocument[], IReviewDocument>).populate({
+    path: 'user',
+    select: 'name photo'
+  })
+  // .populate({
+  //   path: 'tour',
+  //   select: 'name price'
+  // })
+
+  next()
+})
+
 export default mongoose.model<IReviewDocument>('Review', reviewSchema)
