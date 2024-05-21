acdf1b27e5a946bee506bd1af87342a110d8cd88
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-21 10:33:06 +0200

    set NODE_CONFIG_DIR

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config'
+import path from 'path'
 import config from 'config'
 import logger from './utils/logger.utils'
 import createServer from './utils/createServer.utils'
 import connectDb from './utils/connectDB.utils.'
 import errorMiddleware from './middleware/errorMiddleware'
+
+// Set NODE_CONFIG_DIR
+process.env['NODE_CONFIG_DIR'] = path.join(path.resolve('./'), 'config/')
 
 const port = config.get<number>('port')
 
