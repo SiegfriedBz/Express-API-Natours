b95cfadae07e6f8c318ef9f905f4e3f05f5ae940
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-22 12:34:51 +0200

    add logs in setTokenCookieOptions

--- src/utils/setTokenCookieOptions.utils.ts
+++ src/utils/setTokenCookieOptions.utils.ts
@@ -4,6 +4,12 @@ import type { CookieOptions } from 'express'
 export default function setTokenCookieOptions(): CookieOptions {
   const isProduction = process.env.NODE_ENV === 'production'
 
+  console.log('CookieOptions isProduction: ', isProduction)
+  console.log(
+    "CookieOptions config.get<string>('appDomainName'): ",
+    config.get<string>('appDomainName')
+  )
+
   return {
     httpOnly: true,
     path: '/',
